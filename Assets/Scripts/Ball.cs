6fa0b02d4b762270a178600b41f953eebd16b9e2
Bianchi <1lrbianchi@seattleschools.org>
2022-09-23 13:33:56 -0700

    Third

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour{
+    // Movement Speed
+    public float speed = 100.0f;
+    
+    // Start is called before the first frame update
+    void Start() {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) 
+    {  //Hit the Racket?
+                    if (col.gameObject.name == "racket") {
+                        // Calculate hit Factor
+                        float x=hitFactor(transform.position,
+                            col.transform.position,
+                            col.collider.bounds.size.x);
+
+            // Caculate direction, set length to 1
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            //Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        // This function is called whenever the ball
+        // collides with something
+    }
+}
+
+float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth) {
+
+
+    // ascii art:
+    // 
+    // 1  -0.5  0  0.5  1  <- x value
+    // ==================  <- racket
+    //
+    return (ballPos.x - racketPos.x) / racketWidth;
+                }
+                   
+        }
+
+            
+                           
